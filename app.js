73623887eb2b51f99f854a0db4a88aac04b1f6cd
Attila Gyomber <gyomberattila@gmail.com>
2020-02-26 15:27:06 +0100

    env works

--- app.js
+++ app.js
@@ -143,9 +143,23 @@ app.use('/graphql', graphqlHttp({
     graphiql: true
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    app.listen(3000);
-    console.log(" \n Forester NodeJS - GrpaphQL server running! \n");
-}).catch(error => {
-    console.error(error);
-});
+if (process.env.NODE_ENV === 'dev') {
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        app.listen(3000);
+        console.log(" \n Forester NodeJS - GrpaphQL server running! \n");
+    }).catch(error => {
+        console.error(error);
+    });
+};
+if (process.env.NODE_ENV === 'prod') {
+    const MONGO_USER = process.argv[0];
+    const MONGO_PASSWORD = process.argv[1];
+    const MONGO_CLUSTER = process.argv[2];
+    const MONGO_DB = process.argv[3];
+    mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        app.listen(3000);
+        console.log(" \n Forester NodeJS - GrpaphQL server running! \n");
+    }).catch(error => {
+        console.error(error);
+    });
+};
